52e3e07a2a064c4762b342b8a6126647b4cbe18b
Việt Anh Bùi <162571464+AndrewWine@users.noreply.github.com>
2024-11-18 22:00:57 +0700

    ui inventory

---
+++ Assets/Scripts/System/GameManager/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    [SerializeField] private GameObject canvasUI; // Tham chiếu đến Canvas UI
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (canvasUI != null)
+            {
+                // Đổi trạng thái SetActive của Canvas UI
+                canvasUI.SetActive(!canvasUI.activeSelf);
+            }
+        }
+    }
+}
--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -4,28 +4,25 @@ using UnityEngine;
 
 public class UI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Kiểm tra nếu nhấn phím I
+      
     }
 
     public void SwitchTo(GameObject _menu)
     {
-        for(int i = 0;i < transform.childCount;i++)
+        for (int i = 0; i < transform.childCount; i++)
         {
             transform.GetChild(i).gameObject.SetActive(false);
         }
 
-        if(_menu != null)
+        if (_menu != null)
         {
             _menu.SetActive(true);
         }
     }
 }
+    
